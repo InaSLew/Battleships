5a930d637cc4a8d0f334d929ea52c3fb1a2f9414
InaSLew <ina.shenlewander@gmail.com>
2021-10-09 23:41:53 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Battleships
+{
+    
+    class Program
+    {
+        static void Main()
+        {
+            //
+        }
+    }
+
+}
