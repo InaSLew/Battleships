64318ebf5ce59de7f85c9a10db60c0b65292d9dc
InaSLew <ina.shenlewander@gmail.com>
2021-10-10 13:32:06 +0200

    Added PlayerMoveLog

--- Program.cs
+++ Program.cs
@@ -43,11 +43,15 @@ namespace Battleships
             {"", "", "", "", "", "", "", "", "", "", ""},
             {"", "", "", "", "", "", "", "", "", "", ""}
         };
+
+        private static Player currentPlayer;
+        private static List<PlayerMoveLog> moveLog = new List<PlayerMoveLog>();
         
         static void Main()
         {
             Console.WriteLine("Welcome to Battleships!");
             Console.WriteLine();
+            InitializePlayer1();
             UpdatePlayerGrid();
             Console.WriteLine("Where would you like to place your 5X ship?");
             var position = Console.ReadLine();
@@ -57,12 +61,19 @@ namespace Battleships
             UpdatePlayerGrid(ship);
         }
 
+        private static void InitializePlayer1()
+        {
+            currentPlayer = Player.Player1;
+        }
+
         private static void UpdatePlayerGrid(Ship shipToDraw = null)
         {
             var rows = playerGrid.GetLength(0);
             var cols = playerGrid.GetLength(1);
+            var logEntry = new PlayerMoveLog(currentPlayer, shipToDraw);
             for (int i = 0, l = 0; i < rows; i++)
             {
+                // var logEntry = new PlayerMoveLog(currentPlayer, shipToDraw);
                 var tmp = "";
                 for(int j = 0, k = 0; j < cols; j++)
                 {
@@ -81,12 +92,14 @@ namespace Battleships
                     {
                         var startRowPosition = shipToDraw.StartPosition.Item1 + 1;
                         var startColPosition = shipToDraw.StartPosition.Item2 + 1;
+                        var occupiedPosition = (-1, -1, false);
 
                         if (shipToDraw.IsHorizontal)
                         {
                             if (i == startRowPosition && j == startColPosition + k)
                             {
                                 tmp += "X_|";
+                                logEntry.OccupiedPositions.Add((i, j, false));
                                 k++;
                                 if (k == shipToDraw.Size) k = 0;
                             }
@@ -97,17 +110,42 @@ namespace Battleships
                             if (i == startRowPosition + l && j == startColPosition)
                             {
                                 tmp += "X_|";
+                                logEntry.OccupiedPositions.Add((i, j, false));
                                 l++;
                                 if (l == shipToDraw.Size) l = 0;
                             }
                             else tmp += "__|";
                         }
+
+                        // if (occupiedPosition.Item1 != -1 && occupiedPosition.Item2 != -1) logEntry.OccupiedPositions.Add(occupiedPosition);
                     }
                     else tmp += "__|";
                 }
+                // if (logEntry.OccupiedPositions.Any()) moveLog.Add(logEntry);
                 Console.WriteLine(tmp);
             }
+            if (logEntry.Ship != null) moveLog.Add(logEntry);
         }
+    }
+
+    class PlayerMoveLog
+    {
+        public Player Player {get;}
+        public Ship Ship {get;}
+        public List<(int, int, bool)> OccupiedPositions {get; set; }
+
+        public PlayerMoveLog(Player currentPlayer, Ship ship)
+        {
+            Player = currentPlayer;
+            Ship = ship;
+            OccupiedPositions = new List<(int, int, bool)>();
+        }
+    }
+
+    enum Player
+    {
+        Player1,
+        Player2
     }
 
     class Ship
